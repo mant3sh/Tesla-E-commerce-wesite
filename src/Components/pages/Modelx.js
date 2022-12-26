3378617a8991569dd3b0b66b3b03eb039f74e6ee
mant3sh <mahantesha.bv1999@gmail.com>
2022-12-26 20:26:11 +0530

    first commit

--- src/Components/pages/Modelx.js
+++ src/Components/pages/Modelx.js
@@ -3,7 +3,10 @@ import PageFooter from "./pagecomponents/PageFooter";
 import PageHome from "./pagecomponents/PageHome";
 import Autopliot from "./pagecomponents/Autopliot";
 import Pageorder from "./pagecomponents/Pageorder";
-
+import { modelxint, modelxext } from "./pagecomponents/modelxfetures";
+import Features from "./pagecomponents/Features";
+import "./modelxpage.css";
+import { Fade } from "react-awesome-reveal";
 function Modelx() {
   return (
     <div>
@@ -17,11 +20,66 @@ function Modelx() {
         topspeed={"163"}
       />
       <div>
+        <div className="modelxutility">
+          <div className="modelx_utlity_des">
+            <div className="utlity_des">
+              <Fade direction="up" duration={600} fraction="0">
+                <h2
+                  style={{
+                    color: "white",
+                    fontSize: "1.8rem",
+                  }}
+                >
+                  92 ft³
+                </h2>
+                <p
+                  style={{
+                    color: "white",
+                  }}
+                >
+                  Up to 92 ft³ of storage
+                </p>
+              </Fade>
+            </div>
+            <div className="utlity_des">
+              <Fade direction="up" duration={600} fraction="0">
+                <h2
+                  style={{
+                    color: "white",
+                    fontSize: "1.8rem",
+                  }}
+                >
+                  5,000 lbs
+                </h2>
+                <p
+                  style={{
+                    color: "white",
+                  }}
+                >
+                  Tow up to 5,000 lbs
+                </p>
+              </Fade>
+            </div>
+          </div>
+        </div>
+        <Pageorder
+          model={"Utility"}
+          title="Even More Capable"
+          des="With ample storage and 5,000 lbs of towing capacity, Model X is built for maximum utility. Front doors open and close automatically, Falcon Wing rear doors allow for easier loading and a trailer hitch comes standard, so you can bring your gear with you wherever you go."
+        />
+        <Features features={modelxint} />
+
         <Autopliot />
         <Pageorder
           model="Autopilot"
           title={"Future of Driving"}
           des="Autopilot enables your car to steer, accelerate and brake automatically within its lane under your active supervision, assisting with the most burdensome parts of driving. With over-the-air software updates, the latest enhancements are available instantly. Chat with a Tesla Advisor to learn more about Model S or schedule a demo drive today."
+        />
+        <Features features={modelxext} />
+        <Pageorder
+          model="Exterior"
+          title={"Designed for Efficiency"}
+          des="Model X has a drag coefficient of just .24 Cd, the lowest of any production SUV on the planet. Refined aerodynamic elements work together with new wheels and tires to help you travel farther, with sharper handling and better ride comfort."
         />
         <PageFooter image={"modelxpage/modelxfooter"} model="X" />
       </div>
---
+++ src/Components/pages/pagecomponents/modelxfetures.js
@@ -0,0 +1,29 @@
+export const modelxint = [
+  {
+    image: "modelxpage/mx1.jpeg",
+    destitle: "Stay Connected",
+    des: "Instantly connect with multi-device Bluetooth, or fast charge devices with wireless and 36-watt USB-C charging.",
+  },
+  {
+    image: "modelxpage/mx2.jpeg",
+    destitle: "Sublime Sound",
+    des: "A 22-speaker, 960-watt audio system with Active Road Noise Reduction offers the best listening experience wherever you are.",
+  },
+];
+export const modelxext = [
+  {
+    image: "modelxpage/mxe1.jpeg",
+    destitle: "Relentless Performance",
+    des: "Performance wheels and tires keep the SUV planted, transferring even more power down to the road.",
+  },
+  {
+    image: "modelxpage/mxe2.jpeg",
+    destitle: "Optimized Aerodynamics",
+    des: "Attention to detail on all exterior surfaces makes Model X the most aerodynamic production SUV on Earth.",
+  },
+  {
+    image: "modelxpage/mxe3.jpeg",
+    destitle: "Refined Styling",
+    des: "Exterior design combines an iconic look with elegant details.",
+  },
+];
